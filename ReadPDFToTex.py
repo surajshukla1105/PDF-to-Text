0fd56f57ddb9b9318ca7cc4ef2affb6acad7c1ed
Suraj Shukla <surajshukla1105@gmail.com>
2018-03-25 02:54:44 +0800

    Add files via upload

---
+++ ReadPDFToTex.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 15:45:58 2017
+
+@author: Suraj Shukla
+"""
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter#process_pdf
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+
+from io import StringIO
+
+def pdf_to_text(pdfname):
+
+    # PDFMiner boilerplate
+    rsrcmgr = PDFResourceManager()
+    sio = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, sio, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    # Extract text
+    fp = open(pdfname, 'rb')
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+    fp.close()
+
+    # Get text from StringIO
+    text = sio.getvalue()
+
+    # Cleanup
+    device.close()
+    sio.close()
+
+    return text
+
+text = pdf_to_text('D:/Hadoop Data/Aadhar Data Set.pdf')
